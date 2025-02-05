d846db5e0eb0719597d4ba25d1f1ad6020ab13d6
Abhimanyu-13 <abhimanyu@genboot.com>
2025-02-05 18:08:27 +0530

    Resolve some comments

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserCart } from "../Redux/Actions/CartAction/getCartAction";
 import { Link } from "react-router-dom";
 import MotionPath from "../Components/loader";
+import removeItem from "../Images/removeItem.svg";
+import increaseQuantity from "../Images/increaseQuantity.svg";
+import decreaseQuantity from "../Images/decreaseQuantity.svg";
 import { removeItemFromCart } from "../Redux/Actions/CartAction/removeItemAction";
 import { increaseCartItemQuantity } from "../Redux/Actions/CartAction/increaseQuantityAction";
 import { decreaseCartItemQuantity } from "../Redux/Actions/CartAction/decreaseQuantityAction";
@@ -79,14 +82,11 @@ const Cart = () => {
                         onClick={() => handleRemoveItem(product.product._id)}
                         className="mt-6 font-semibold text-red-500 text-xs flex items-center gap-1 shrink-0"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
+                        <img
+                          src={removeItem}
                           className="w-4 fill-current inline"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"></path>
-                          <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"></path>
-                        </svg>
+                          alt=""
+                        />
                         REMOVE
                       </button>
                     </div>
@@ -100,27 +100,24 @@ const Cart = () => {
                       type="button"
                       className="mt-6 flex items-center px-3 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
+                      <img
+                        src={decreaseQuantity}
                         className="w-2.5 fill-current"
-                        viewBox="0 0 124 124"
                         onClick={() =>
                           handleDecreaseCartItemQuantity(product.product._id)
                         }
-                      >
-                        <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"></path>
-                      </svg>
+                        alt=""
+                      />
+
                       <span className="mx-3 font-bold">{product.quantity}</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
+                      <img
+                        src={increaseQuantity}
                         className="w-2.5 fill-current"
-                        viewBox="0 0 42 42"
                         onClick={() =>
                           handleIncreaseCartItemQuantity(product.product._id)
                         }
-                      >
-                        <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"></path>
-                      </svg>
+                        alt=""
+                      />
                     </button>
                   </div>
                 </div>

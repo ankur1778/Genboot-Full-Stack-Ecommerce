b641c6498643be688a5454fabb2cb1ddf0cc7371
Abhimanyu-13 <abhimanyu@genboot.com>
2025-03-04 13:13:29 +0530

    make some changes

--- src/AdminComponent/AdminNavbar.jsx
+++ src/AdminComponent/AdminNavbar.jsx
@@ -5,21 +5,25 @@ import { Link } from "react-router-dom";
 const AdminNavbar = ({ handleChange }) => {
   const navClass =
     "px-4 py-2 text-white text-xl cursor-pointer hover:bg-gray-200 hover:text-red-800 hover:scale-110 rounded-lg";
-  const navLinks = ["userManagement", "productManagement", "orderManagement"];
+  const navArray = [
+    { id: 1, value: "User Management", function: "userManagement" },
+    { id: 2, value: "Product Management", function: "productManagement" },
+    { id: 3, value: "Order Management", function: "orderManagement" },
+  ];
   return (
     <nav className="bg-slate-600 rounded-lg ml-5 h-auto w-64 md:w-56 lg:w-64 font-bold shadow-lg fixed md:relative z-20">
       <div className="flex justify-center my-6">
         <img className="h-16 w-20 mt-10" src={logo} alt="Logo" />
       </div>
       <ul className="space-y-4 text-center">
-        {navLinks.map((nav, index) => {
+        {navArray.map((el) => {
           return (
             <li
-              key={index}
+              key={el.id}
               className={navClass}
-              onClick={() => handleChange(nav)}
+              onClick={() => handleChange(el.function)}
             >
-              {nav}
+              {el.value}
             </li>
           );
         })}
